23186865c587347ed0026e1eb9797440d3a9ab2c
ManuelGTM <manuelgabrielt@gmail.com>
2024-12-29 19:41:19 -0300

    bluthoot

--- config/34keys.h
+++ config/34keys.h
@@ -5,7 +5,8 @@
 #define BASE 0
 #define NAV 1
 #define SYM 2
+#define GAMES 3
 
 // Home row mods macro
-#define HRML(k1,k2,k3,k4) &ht LGUI k1  &ht LALT k2  &ht LCTRL k3  &ht LSHFT k4
-#define HRMR(k1,k2,k3,k4) &ht RSHFT k1  &ht RCTRL k2  &ht RALT k3  &ht RGUI k4
+#define HRML(k1, k2, k3, k4) &ht LSHFT k1 &ht LALT k2 &ht LCTRL k3 &ht LGUI k4
+#define HRMR(k1, k2, k3, k4) &ht RGUI k1 &ht RCTRL k2 &ht RALT k3 &ht RSHFT k4
