2ae7bc2c3c707546862af4893341d222684e8099
ManuelGTM <manuelgabrielt@gmail.com>
2025-12-14 20:34:23 -0300

    Fixing this shiet

--- config/34keys.h
+++ config/34keys.h
@@ -9,5 +9,5 @@
 #define GAMES 4
 
 // Home row mods macro
-//#define HRML(k1, k2, k3, k4) &ht LGUI k1 &ht LALT k2 &ht LCTRL k3 &ht LSHFT k4
-//#define HRMR(k1, k2, k3, k4) &ht RSHFT k1 &ht RCTRL k2 &ht RALT k3 &ht RGUI k4
+#define HRML(k1, k2, k3, k4) &ht LGUI k1 &ht LALT k2 &ht LCTRL k3 &ht LSHFT k4
+#define HRMR(k1, k2, k3, k4) &ht RSHFT k1 &ht RCTRL k2 &ht RALT k3 &ht RGUI k4
